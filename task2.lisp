1a7e0dc707712022c2bc50ca53e001e35680e736
chucherov <31537725+chucherov@users.noreply.github.com>
2019-12-26 13:26:54 +0300

    Update and rename task2.list to task2.lisp

--- task2.lisp
+++ task2.lisp
@@ -1,5 +1,5 @@
 ;; Чтение файла
-(let ((in (open "test.txt" :if-does-not-exist nil)))
+(let ((in (open path :if-does-not-exist nil)))
     (when in
         (loop for line = (read-line in nil)
     while line do (format t "~a~%" line))
